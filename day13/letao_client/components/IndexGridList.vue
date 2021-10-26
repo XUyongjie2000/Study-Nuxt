eab751aaeb878877fea01dd5c82ba252317ce6cc
XUyongjie2000 <99706807@qq.com>
2021-10-26 08:47:37 +0800

    props

--- day13/letao_client/components/IndexGridList.vue
+++ day13/letao_client/components/IndexGridList.vue
@@ -1,21 +1,14 @@
 <template>
   <div class="grid">
     <van-grid :border="false" :column-num="3">
-      <van-grid-item>
-        <van-image src="https://img01.yzcdn.cn/vant/apple-2.jpg" />
+      <van-grid-item v-for="item in gridlist" :key="item.id">
+        <van-image :src="item.img_src" />
       </van-grid-item>
-      <van-grid-item>
-        <van-image src="https://img01.yzcdn.cn/vant/apple-3.jpg" />
-      </van-grid-item>
-      <van-grid-item>
-        <van-image src="https://img01.yzcdn.cn/vant/apple-1.jpg" />
-      </van-grid-item>
-      <van-grid-item>
-        <van-image src="https://img01.yzcdn.cn/vant/apple-2.jpg" />
-      </van-grid-item>
-      <van-grid-item>
-        <van-image src="https://img01.yzcdn.cn/vant/apple-3.jpg" />
-      </van-grid-item> 
     </van-grid>
   </div>
 </template>
+<script>
+export default {
+  props: ["gridlist"]
+};
+</script>
