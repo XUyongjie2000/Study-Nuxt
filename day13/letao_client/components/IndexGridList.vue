99db8362a696c4206fe526e5c7ceab8126adb198
XUyongjie2000 <99706807@qq.com>
2021-10-26 08:28:39 +0800

    服务端

---
+++ day13/letao_client/components/IndexGridList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="grid">
+    <van-grid :border="false" :column-num="3">
+      <van-grid-item>
+        <van-image src="https://img01.yzcdn.cn/vant/apple-2.jpg" />
+      </van-grid-item>
+      <van-grid-item>
+        <van-image src="https://img01.yzcdn.cn/vant/apple-3.jpg" />
+      </van-grid-item>
+      <van-grid-item>
+        <van-image src="https://img01.yzcdn.cn/vant/apple-1.jpg" />
+      </van-grid-item>
+      <van-grid-item>
+        <van-image src="https://img01.yzcdn.cn/vant/apple-2.jpg" />
+      </van-grid-item>
+      <van-grid-item>
+        <van-image src="https://img01.yzcdn.cn/vant/apple-3.jpg" />
+      </van-grid-item> 
+    </van-grid>
+  </div>
+</template>
---
+++ day13/letao_client/pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <!-- 1.轮播图组件 -->
+    <IndexCarousel :swiperList="swiperList" />
+    <!-- 2.宫格组件 -->
+    <IndexGridList :gridlist="gridlist" />
+    <!-- 3.活动组件 -->
+    <IndexActive />
+    <!-- 4.品牌组件 -->
+    <IndexBrand />
+    <!-- 5.运动专区 -->
+    <IndexSports :sports="sports" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    // const { swiperList } = await $axios.$get("/banners");
+    // console.log(swiperList, "swiperList");
+    //轮播图接口
+    const { swiperList } = await $axios.$get("/banners");
+    const { gridlist } = await $axios.$get("/gridlist");
+    const { sports } = await $axios.$get("/sports");
+    return {
+      swiperList,
+      gridlist,
+      sports
+    };
+  }
+};
+</script>
+
+<style></style>
